96b241da220a16c845e2e577202ad481b7852f52
Archie To <toanhminh0412@gmail.com>
2023-07-18 18:47:04 -0700

    Make a small change in header styling

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <div className="hero h-96" style={{backgroundImage: 'url(/images/header.jpg)'}}>
+        <div className="hero min-h-[24rem]" style={{backgroundImage: 'url(/images/header.jpg)'}}>
             <div className="hero-overlay bg-opacity-70 bg-black"></div>
             <div className="hero-content text-center">
-                <div className="max-w-xl lg:max-w-4xl prose text-white">
+                <div className="max-w-xl lg:max-w-4xl prose text-white py-10">
                     <h1 className="mb-5 text-5xl font-bold text-white">BTG - Immigration consulting</h1>
                     <p className="mb-5 text-xl font-light">We help you with the complicated protocols so you can begin your journey to Canada today!</p>
                     <Link href="#" className="btn no-underline me-1">Services</Link>
