4f21a3dad67966f1016003f506351c4ee7a16de1
Archie To <toanhminh0412@gmail.com>
2023-07-18 18:30:22 -0700

    Fix linitng errors

--- src/app/components/AboutMe.js
+++ src/app/components/AboutMe.js
@@ -12,24 +12,24 @@ export default function AboutMe() {
                     </div>
                     <p>
                     Our founder Ama is a Regulated Canadian Immigration Consultant(RCIC) located in beautiful Victoria  British Columbia. As an immigrant herself who came to Canada for a better future, she understands firsthand how overwhelming, confusing, and time-consuming the immigration process can be to navigate-not to mention how isolating it can feel when doing it alone. 
-                    Since the past 14 years she has been here, she's been lucky enough to meet people from all kinds of backgrounds, ethnicities, and cultures-all with their own Canadian immigration stories.
+                    Since the past 14 years she has been here, she&apos;s been lucky enough to meet people from all kinds of backgrounds, ethnicities, and cultures-all with their own Canadian immigration stories.
                     And in hearing about their experiences not meeting their expectations, it became clear that there was a need for a professional service that could help other immigrants navigate the complex process of getting settled in Canada.
                     </p>
                     <div className="relative w-80 aspect-square sm:float-right mt-8 sm:mt-0 md:ms-8 mb-8 not-prose">
                         <Image src="/images/pic-2.jpg" fill style={{objectFit: "contain"}} alt="Profile picture 2"/>
                     </div>
                     <p>
-                    Ama is passionate about helping individuals, families, and businesses, achieve their Canadian immigration dreams. She knows how difficult it can sometimes be to sort through all of the information out there, especially when you're feeling overwhelmed by the ever fluid Immigration programs and process. 
-                    That's why BTG Immigration Consulting was created to eliminate that sense of isolation and confusion,  and provide a secure,  engagkng and collaborative service. Our goal is to make the process as simple, straightforward, and painless as possible.
+                    Ama is passionate about helping individuals, families, and businesses, achieve their Canadian immigration dreams. She knows how difficult it can sometimes be to sort through all of the information out there, especially when you&apos;re feeling overwhelmed by the ever fluid Immigration programs and process. 
+                    That&apos;s why BTG Immigration Consulting was created to eliminate that sense of isolation and confusion,  and provide a secure,  engagkng and collaborative service. Our goal is to make the process as simple, straightforward, and painless as possible.
                     </p>
                 </article>
                 <article>
                     <h2>Our Mission</h2>
                     <p>
-                    When you work with BTG Immigration Consulting, you will be assigned an expert who will guide you through every step of the immigration process. No matter what your goals are whether they're personal or professional, we're here for you at BTG 
+                    When you work with BTG Immigration Consulting, you will be assigned an expert who will guide you through every step of the immigration process. No matter what your goals are whether they&apos;re personal or professional, we&apos;re here for you at BTG.
                     Our mission is to provide our clients with a secure, transparent, and seamless service; one that is easy to understand and stress-free. We strive to give our clients the best possible experience by working together collaboratively.
                     BTG Immigration Consulting operates with one goal: to provide a seamless, secure, and transparent service; one that is easy to understand for families, businesses,  individuals, seeking concise answers to begin their immigration journey to Canada and looking for an expert partner in navigating the Canadian immigration process. 
-                    You shouldn't have to be an expert just to get your foot in the door. With BTG on your side, you won't have to be.
+                    You shouldn&apos;t have to be an expert just to get your foot in the door. With BTG on your side, you won&apos;t have to be.
                     </p>
                 </article>
                 <div className="text-center mt-8">
