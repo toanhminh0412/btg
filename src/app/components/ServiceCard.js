692ba8da8c4ba41f3d853b4a9d0221abd5871f00
Archie To <toanhminh0412@gmail.com>
2023-07-26 08:55:35 -0700

    Latest updates

--- src/app/components/ServiceCard.js
+++ src/app/components/ServiceCard.js
@@ -10,7 +10,7 @@ export default function ServiceCard({service}) {
         <div className={`card bg-base-100 shadow-xl border border-slate-200 ${open ? 'lg:row-span-3' : ''}`}>
             <div className="card-body">
                 <div className="prose max-w-none">
-                    <h2 className="card-title"><span dangerouslySetInnerHTML={{__html: service.title}}/> <span className="ms-auto text-base font-light float-right cursor-default hover:font-semibold duration-300 whitespace-nowrap ps-4" onClick={() => (setOpen(!open))}>{open ? <span className="text-xl">&uarr;</span> : <span className="text-2xl">&darr;</span>}</span></h2>
+                    <h2 className="card-title text-lg lg:text-xl"><span dangerouslySetInnerHTML={{__html: service.title}}/> <span className="ms-auto text-base font-light float-right cursor-default hover:font-semibold duration-300 whitespace-nowrap ps-4" onClick={() => (setOpen(!open))}>{open ? <span className="text-xl">&uarr;</span> : <span className="text-xl">&darr;</span>}</span></h2>
                     {open ? 
                     <div>
                         {service.image ? 
