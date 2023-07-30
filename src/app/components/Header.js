e542a5f875cd5bca24186b736a6b7f4fb5a91a7d
Minh To <toanhminh0412@gmail.com>
2023-07-29 22:10:40 -0700

    Add some z-indexes

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,11 +4,11 @@ export default function Header() {
     return (
         <div>
         <div className="hero min-h-[24rem]">
-            <video playsInline autoPlay muted loop id="myVideo" className="h-full object-cover">
+            <video playsInline autoPlay muted loop id="myVideo" className="h-full object-cover z-10">
                 <source src="https://firebasestorage.googleapis.com/v0/b/btg-immigration-consulting.appspot.com/o/header.mp4?alt=media&token=c152860a-b91f-4f4a-af79-a3925cda52ee" type="video/mp4"/>
             </video>
-            <div className="hero-overlay bg-opacity-70 bg-black"></div>
-            <div className="hero-content text-center">
+            <div className="hero-overlay bg-opacity-70 bg-black z-20"></div>
+            <div className="hero-content text-center z-30">
                 <div className="max-w-xl lg:max-w-4xl prose text-white py-10">
                     <h1 className="mb-5 text-xl lg:text-3xl font-bold text-white">Welcome to BTG Immigration Consulting - Your expert partner for Canadian Immigration!</h1>
                     {/* <h2 className="mb-7 text-lg lg:text-2xl font-normal text-white mt-[5px]">We are better together</h2> */}
