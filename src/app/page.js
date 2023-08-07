f11b23f147eb8a42759962d2dfe8fe0e48471972
Archie To <toanhminh0412@gmail.com>
2023-08-07 09:38:55 -0700

    Add contact us form

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
+import Script from "next/script"
 import AboutMe from "./components/AboutMe"
-import Header from "./components/Header"
 import Services from "./components/Services"
 
 export const metadata = {
@@ -11,6 +11,7 @@ export default function Home() {
     <div>
       <AboutMe />
       <Services />
+      <Script src="https://www.cognitoforms.com/f/seamless.js" data-key="TaJuz4Nzc0aJoUcgy2rbCQ" data-form="1"/>
     </div>
   )
 }
